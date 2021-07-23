72384c3f8ffed0c6f3b0f0468d38190b5617958f
unknown <tranducanh@sace.edu.vn>
2021-07-23 21:36:25 +0700

    First commit

---
+++ Buoi 7/buoi7.js
@@ -0,0 +1,11 @@
+let firstname = "Tran"
+let surname = "Duc"
+let lastname ="Anh"
+console.log(firstname +" "+ surname +" "+ lastname);
+alert(firstname +" "+ surname +" "+ lastname);
+
+let calculation = 12 % 5
+console.log(calculation);
+
+let my_name = [firstname, surname, lastname]
+console.log(my_name.length)
---
+++ Buoi6/buoi6.js
@@ -0,0 +1,9 @@
+// var x = 1
+// let chu ="duc"
+// const SO = 1.5
+// // rcm dùng let và const
+// console.log("minh duc")
+
+let ten = "Duc Anh"
+let age = 17
+console.log(ten,"nam nay" ,age ,"tuoi.")
